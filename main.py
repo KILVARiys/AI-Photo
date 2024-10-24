ad5ae3b7fffc5dd7875cd95214c0d48df244b793
Admin <seremenko135@gmail.com>
2024-10-24 19:29:07 +0300

    Last

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from diffusers import AutoPipelineForText2Image
 import torch
+import os
+# Импортируем модуль для генерации уникальных идентификаторов
+import uuid
 
 # Загрузка модели на процессор
 pipeline = AutoPipelineForText2Image.from_pretrained(
@@ -7,10 +10,22 @@ pipeline = AutoPipelineForText2Image.from_pretrained(
 ).to("cpu")  # Изменено с "cuda" на "cpu"
 
 # Генерация изображения
+prompt = input('Enter the text from which the picture will be created (Example - Astronaut in the jungle, cold color palette, muted colors, detail, 8k):\n')
+
 image = pipeline(
-    prompt="Astronaut in a jungle, cold color palette, muted colors, detailed, 8k",
+    prompt=prompt,
     negative_prompt="ugly, deformed, disfigured, poor details, bad anatomy",
 ).images[0]
 
+# Папка загрузок
+downloads_folder = os.path.join(os.path.expanduser("~"), "Downloads")
+
+# Генерация случайного названия файла
+random_filename = f"{uuid.uuid4()}.png"
+image_path = os.path.join(downloads_folder, random_filename)
+
 # Сохранение изображения
-image.save("FHOTO.png")
+image.save(image_path)
+
+# Пишем путь сохранения файла
+print(f"Image saved to {image_path}")
